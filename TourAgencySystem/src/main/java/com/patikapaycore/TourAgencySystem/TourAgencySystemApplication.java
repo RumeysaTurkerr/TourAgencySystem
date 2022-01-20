243df79783d60553d651aa4298b628a66d263587
Rumeysa Turker <rumeysatur96@gmail.com>
2022-01-20 12:57:00 +0300

    project base initialize

---
+++ TourAgencySystem/src/main/java/com/patikapaycore/TourAgencySystem/TourAgencySystemApplication.java
@@ -0,0 +1,13 @@
+package com.patikapaycore.TourAgencySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourAgencySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourAgencySystemApplication.class, args);
+	}
+
+}
---
+++ TourAgencySystem/src/test/java/com/patikapaycore/TourAgencySystem/TourAgencySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patikapaycore.TourAgencySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourAgencySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
