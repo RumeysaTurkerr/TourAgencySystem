a3f4a2d9cdb639eedac3da3b1ad789ee5bccf46f
Rumeysa Turker <rumeysatur96@gmail.com>
2022-02-15 17:38:27 +0300

    data.sql updated; exception,mapper packages are added. application.properties file updated. HotelDto object is created and necessary implementations done.

--- TourAgencySystem/src/main/java/com/patikapaycore/TourAgencySystem/model/Hotel.java
+++ TourAgencySystem/src/main/java/com/patikapaycore/TourAgencySystem/model/Hotel.java
@@ -1,19 +1,46 @@
 package com.patikapaycore.TourAgencySystem.model;
+import com.patikapaycore.TourAgencySystem.model.Address;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class Hotel {
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "hotel")
+public class Hotel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull(message = "{validation.messages.hotel.name}")
     private String name;
+    @NotNull(message = "{validation.messages.hotel.type}")
+    private String type;
+    @NotNull(message = "{validation.messages.hotel.address}")
+    private String address;
 
-    public Hotel(String name) {
-        this.name = name;
-    }
+    @Transient
+    private List<Address> addresses;
 
-    public Hotel(){}
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public List<Address> formatToAddressList() {
+        String[] splittedAddresses = address.split("//");
+        List<Address> resultFormat = new ArrayList<>();
+        Arrays.stream(splittedAddresses).forEach(split -> {
+            String[] splitteds = split.trim().split("/");
+            resultFormat.add(new Address(splitteds[0], splitteds[1]));
+        });
+        return resultFormat;
     }
 }
--- TourAgencySystem/src/main/java/com/patikapaycore/TourAgencySystem/service/iml/HotelServiceImpl.java
+++ TourAgencySystem/src/main/java/com/patikapaycore/TourAgencySystem/service/iml/HotelServiceImpl.java
@@ -1,51 +1,104 @@
 package com.patikapaycore.TourAgencySystem.service.iml;
-
+import com.patikapaycore.TourAgencySystem.exception.NotFoundException;
+import com.patikapaycore.TourAgencySystem.model.Address;
 import com.patikapaycore.TourAgencySystem.model.Hotel;
+import com.patikapaycore.TourAgencySystem.repository.HotelRepository;
 import com.patikapaycore.TourAgencySystem.service.HotelService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 @Service
+@RequiredArgsConstructor
 public class HotelServiceImpl implements HotelService {
-    private List<Hotel> hotels= new ArrayList<>();
-    {
-        hotels.add(new Hotel("Hotel 1"));
-        hotels.add(new Hotel("Hotel 2"));
-    }
+
+    private final HotelRepository hotelRepository;
+
     @Override
     public List<Hotel> getAllHotels() {
-        return getAllHotels();
+        return hotelRepository.findAll();
     }
 
     @Override
     public Hotel getHotel(Integer id) {
-        return null;
+        Optional<Hotel> byId = hotelRepository.findById(id);
+        return byId.orElseThrow(() -> new NotFoundException("Hotel"));
     }
 
     @Override
-    public boolean addHotel(Hotel hotel) {
-      return hotels.add(hotel);
+    public void addHotel(Hotel hotel) {
+        hotelRepository.save(hotel);
     }
 
     @Override
-    public Hotel updateHotel(String name, Hotel hotel) {
-        AtomicBoolean status=new AtomicBoolean(false);
-    hotels.forEach(hotelItem->{
-        if(hotelItem.getName().equals(name)){
-            status.set(true);
-            hotelItem.setName(hotel.getName());
-        }
-    });
-    if(status.get())
-        return hotel;
-    return null;
+    public Hotel updateHotel(Integer id, Hotel hotel) {
+        getHotel(id);
+        hotel.setId(id);
+        return hotelRepository.save(hotel);
     }
 
     @Override
     public boolean deleteHotel(Integer id) {
-        return false;
+        hotelRepository.delete(getHotel(id));
+        return true;
     }
+
+    // java8 playground again :)
+    // ----------------------------------------------------------------------------------
+    private List<Address> getAddressCityStartsWith(String prefix) {
+        List<Hotel> allAirports = getAllHotels();
+        return allAirports.stream()
+                .map(Hotel::getAddresses)
+                .flatMap(Collection::stream)
+                .distinct()
+                .filter(a -> a.getCity().startsWith(prefix))
+                .collect(Collectors.toList());
+    }
+
+    private void printAllAdressCityStartsWith(String prefix) {
+        List<Address> addressCityStartsWith = getAddressCityStartsWith(prefix);
+        addressCityStartsWith.stream()
+                .map(address -> address.getCity() + "/" + address.getStreetCode() + "/" +
+                        address.getBuildingNo())
+                .distinct()
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+
+    private void reduceAddressListToCityNameAndStreetCode() {
+        List<Hotel> allAirports = getAllHotels();
+        String reducedAddressList = allAirports.stream()
+                .map(Hotel::getAddresses)
+                .flatMap(Collection::stream)
+                .map(address -> address.getCity() + " " + address.getStreetCode())
+                .reduce("", (s1, s2) -> s1 + s2);
+
+        System.out.println("Reduced address List : " + reducedAddressList);
+    }
+
+    private String getCombinedAddressOfBoth(Hotel airport1, Hotel airport2) {
+        // Airports check here!
+        // I assume that there is nothing bad here :) and enjoy with BiFunction sample
+        BiFunction<Hotel, Hotel, String> function = (a1, a2) -> a1.getAddresses().get(0).getCity() + "-" + a1.getAddresses().get(0).getStreetCode()
+                + " ------- " +
+                a2.getAddresses().get(0).getCity() + "-" + a2.getAddresses().get(0).getStreetCode();
+
+        // Gets combined Address String
+        return function.apply(airport1, airport2);
+    }
+
+    private void consumeHotelAddresses(Integer airportId) {
+        Hotel airport = hotelRepository.getById(airportId);
+
+        // Defining a consumer for airport address list
+        Consumer<Hotel> airportConsumer = air -> air.getAddresses().forEach(System.out::println);
+
+        airportConsumer.accept(airport);
+    }
+
 }
